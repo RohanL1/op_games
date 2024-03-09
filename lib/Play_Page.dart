f6dba1671c0a9e338142553327a1abf0d76d5f4e
Sai Nikhil <katarusai06@gmail.com>
2024-03-09 08:04:28 -0800

    Committing changes to Play_Page.dart and pubspec.yaml

--- lib/Play_Page.dart
+++ lib/Play_Page.dart
@@ -2,6 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:op_games/OpMatchGame.dart';
 import 'package:op_games/Quiz_page.dart';
 import 'package:op_games/level2_quiz_screen.dart';
+import 'package:op_games/Score_page.dart';
+
+class LevelSelectionContainer extends StatelessWidget {
+  final String levelText;
+  final Widget nextPage;
+  final Color startColor;
+  final Color endColor;
+
+  const LevelSelectionContainer({
+    Key? key,
+    required this.levelText,
+    required this.nextPage,
+    this.startColor = Colors.greenAccent,
+    this.endColor = Colors.white,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(builder: (context) => nextPage));
+      },
+      borderRadius: BorderRadius.circular(30),
+      child: Container(
+        width: 150,
+        height: 150,
+        padding: const EdgeInsets.all(10),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(30),
+          gradient: LinearGradient(
+            colors: [startColor, endColor],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              levelText,
+              style: TextStyle(color: Colors.green, fontWeight: FontWeight.bold, fontSize: 40),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
 
 class PlayPage extends StatelessWidget {
   const PlayPage({Key? key}) : super(key: key);
@@ -9,64 +57,93 @@ class PlayPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Play',
-            style: TextStyle(fontFamily: 'Playful', color: Colors.white)),
-        flexibleSpace: Container(
-          decoration: BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Colors.black, // Start color
-                Colors.white, // End color
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-        ),
-        backgroundColor: Colors.transparent,
-        // Make the AppBar's background transparent to show the gradient
-        elevation: 0, // Remove shadow
-        iconTheme: IconThemeData(
-          color: Colors.white, // Choose your color here
-        ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.pop(context);
+        },
+        foregroundColor: Colors.black,
+        backgroundColor: Colors.lightBlue,
+        shape: CircleBorder(),
+
+        child: const Icon(Icons.arrow_back_ios),
       ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: AssetImage('assets/home_screen.png'),
-            fit: BoxFit.cover,
-          ),
-        ),
-        child: Center(
-          child: Padding(
-            padding: const EdgeInsets.all(160.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                levelCard(
-                    context, 'Level 0 -   Basic Match Game ', Icons.looks_one,
-                    OperatorsMatchingGamePage()),
-                levelCard(
-                    context, 'Level 1 -   Quiz ', Icons.looks_two, QuizPage()),
-                levelCard(
-                    context, 'Level 2 -   Fill the literals ', Icons.looks_3,
-                    PlayScreen()),
-              ],
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('assets/background.png'),
+              fit: BoxFit.cover,
             ),
           ),
-        ),
+          child: Center(
+              child: Column(
+                children: [
+                  SizedBox(height: 40),
+                  Text('LEVELS',
+                    style: TextStyle(color: Colors.black87, fontWeight: FontWeight.bold, fontSize: 50),
+                  ),
+                  SizedBox(height: 70),
+                  Row(
+                      children :[
+                        SizedBox(width: 70),
+                        LevelSelectionContainer(levelText: 'LVL 0', nextPage: OperatorsMatchingGamePage()),
+
+                        SizedBox(width: 50),
+                        LevelSelectionContainer(levelText: 'LVL 1', nextPage: QuizPage()),
+
+                        SizedBox(width: 50),
+                        LevelSelectionContainer(levelText: 'LVL 2', nextPage: PlayScreen()),
+
+                        SizedBox(width: 50),
+                        LevelSelectionContainer(levelText: 'LVL 3', nextPage: ScorePage()),
+
+                        SizedBox(width: 50),
+                        LevelSelectionContainer(levelText: 'LVL 4', nextPage: ScorePage()),
+
+                        SizedBox(width: 50),
+                        LevelSelectionContainer(levelText: 'LVL5', nextPage: ScorePage()),
+
+                      ]
+                  ),
+                  SizedBox(height: 70),
+                  Row(
+                      children :[
+                        SizedBox(width: 70),
+                        LevelSelectionContainer(levelText: 'LVL 6', nextPage: ScorePage()),
+
+                        SizedBox(width: 50),
+                        LevelSelectionContainer(levelText: 'LVL 7', nextPage: ScorePage()),
+
+                        SizedBox(width: 50),
+                        LevelSelectionContainer(levelText: 'LVL 8', nextPage: ScorePage()),
+
+                        SizedBox(width: 50),
+                        LevelSelectionContainer(levelText: 'LVL 9', nextPage: ScorePage()),
+
+                        SizedBox(width: 50),
+                        LevelSelectionContainer(levelText: 'LVL 10', nextPage: ScorePage()),
+
+                        SizedBox(width: 50),
+                        LevelSelectionContainer(levelText: 'LVL 11', nextPage: ScorePage()),
+
+                      ]
+                  ),
+                ],
+              )
+          )
       ),
+
     );
   }
+
 
   Widget levelCard(BuildContext context, String level, IconData icon,
       Widget nextPage) {
     return Card(
       margin: EdgeInsets.only(bottom: 20),
       elevation: 10,
-      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
       child: InkWell(
         onTap: () {
           Navigator.push(context, MaterialPageRoute(
